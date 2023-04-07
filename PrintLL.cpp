9c80cb4c2348b289189ed20aeafb212c7d281a8e
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-04-07 06:41:39 +0530

    Create PrintLL.cpp

---
+++ PrintLL.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+    public:
+    void display(Node *head)
+    {
+      //your code goes here
+      Node *curr=head;
+      while(curr!=NULL){
+          cout<<curr->data<<" ";
+          curr=curr->next;
+      }
+    }
+};
