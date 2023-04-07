a9d96a35ecbe744ccbbcb116dd143625c07a78e2
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-04-07 07:07:36 +0530

    Create Insert-middle-ll.cpp

---
+++ Insert-middle-ll.cpp
@@ -0,0 +1,14 @@
+//Function to insert a node in the middle of the linked list.
+Node* insertInMiddle(Node* head, int x)
+{
+	Node* slow=head;
+	Node* fast=head;
+	while(fast->next!=NULL && fast->next->next!=NULL){
+	    slow=slow->next;
+	    fast=fast->next->next;
+	}
+	Node* temp=new Node(x);
+	temp->next=slow->next;
+	slow->next=temp;
+	return head;
+}
