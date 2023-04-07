62281af7731a57c0b44b88ed660b529b75679158
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-04-07 06:26:36 +0530

    Create Count-nodes-ll.cpp

---
+++ Count-nodes-ll.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+    public:
+    //Function to count nodes of a linked list.
+    int getCount(struct Node* head){
+        int count=0;
+        Node* curr=head;
+        while(curr!=NULL){
+            count++;
+            curr=curr->next;
+        }
+        return count;
+    }
+};
+    
